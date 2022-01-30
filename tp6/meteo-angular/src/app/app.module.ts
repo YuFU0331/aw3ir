c4f790b9c9d99386aa75a9ae1cc72500b13e7ddd
YuFU0331 <92270449+YuFU0331@users.noreply.github.com>
2022-01-30 03:28:49 +0100

    Update app.module.ts

--- tp6/meteo-angular/src/app/app.module.ts
+++ tp6/meteo-angular/src/app/app.module.ts
@@ -1,38 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeteoComponent } from './meteo/meteo.component';
+import { RouterModule, Routes } from '@angular/router';
 import { MeteoDetailComponent } from './meteo-detail/meteo-detail.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
- // <--  Ajouter les références ici
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MeteoComponent,
-  
+    
   ],
   imports: [
+    
     BrowserModule,
-    AppRoutingModule,
-    FormsModule, ReactiveFormsModule
+    FormsModule, ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-
-@NgModule({
-  imports: [
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
-    // autres imports ici
-  ],
-  ...
-})
+  
 
 const appRoutes: Routes = [
   { 
@@ -49,7 +39,14 @@ const appRoutes: Routes = [
   }
 ];
 
-
-
-
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } 
+    )
+  ],
+  ...
+})
 export class AppModule { }
+  
